8a812aeb8a918c8930a940527e39cc29f5be0b84
qwopqwop200 <qwopqwop200@gmail.com>
2023-09-15 11:22:03 +0900

    update version

--- auto_quant/__init__.py
+++ auto_quant/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 from .models import AutoQuantForCausalLM, AutoQuantConfig
 from .quant.awq.models import AutoAWQForCausalLM, AWQConfig, BaseAWQForCausalLM
 from .quant.gptq.models import AutoGPTQForCausalLM, GPTQConfig, BaseGPTQForCausalLM
